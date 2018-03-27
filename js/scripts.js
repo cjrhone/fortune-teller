f4c44d4341acd2a7af80f59dc556211249a9e41e
Alex Noble and CJ Rhone <cj@>
2018-03-27 11:14:10 -0700

    Add functionality for quiz, displaying results

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,29 @@
+
+
 $(document).ready(function() {
- $("form#fortuneQuiz").submit(function(event) {
-   event.preventDefault();
+  $("form#fortuneQuiz").submit(function(event) {
+    event.preventDefault();
 
-   $("#badResult").show();
-   $("#goodResult").show();
+    $(".result").fadeOut(200);
 
- })
+    var score=0;
+    $("input:checkbox[name=bad-luck]:checked").each(function(){
+      score--;
+    });
+    $("input:checkbox[name=good-luck]:checked").each(function(){
+      score++;
+    });
 
-})
+
+    if (score>=1){
+      $("#goodResult").show();
+    } else if(score===0) {
+      $("#neutralResult").show();
+    } else{
+      $("#badResult").show();
+    }
+
+
+ });
+
+});
